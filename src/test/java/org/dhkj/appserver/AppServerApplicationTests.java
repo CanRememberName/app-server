ee6b962da60aa6aea086d8f6473dd23391098f7e
dihuangkaijia <1134847027@qq.com>
2023-04-08 21:53:28 +0800

    首次提交

---
+++ src/test/java/org/dhkj/appserver/AppServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dhkj.appserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
